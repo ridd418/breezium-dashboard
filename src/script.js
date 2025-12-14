eccde42aa4d3473cdf297eca19d0e53052a79d78
Ridd418 <147160638+ridd418@users.noreply.github.com>
2025-12-14 23:11:52 +0530

    A2: Added clock, Updated CSS and HTML

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
 document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1550292554-7e53d78fd7a5?ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NjQwODkxNjR8&ixlib=rb-4.1.0)`
+
+// ********************************Clock********************************
+const rendertime = () => {
+    const clock = document.getElementById('clock')
+
+    const updateTime = () => {
+        clock.textContent = new Date().toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        })
+
+    }
+
+    updateTime()
+
+    setInterval(updateTime, 1000)
+}
+
+rendertime()

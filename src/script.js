5b2279dcc77eb17b3212c5c5d07d979e3d73503b
Ridd418 <147160638+ridd418@users.noreply.github.com>
2025-12-15 00:45:20 +0530

    A3: Added dynamic background, Added gitignore to exclude the local test_assets

--- src/script.js
+++ src/script.js
@@ -1,4 +1,58 @@
-document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1550292554-7e53d78fd7a5?ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NjQwODkxNjR8&ixlib=rb-4.1.0)`
+// ********************************Dynamic Backgrounds********************************
+const setBackground = () => {
+
+    // Pull Image List and URL from JSON using fetch (Later!)
+    const imageList = {
+        "0" : "dear-1.jpg",
+        "2" : "dear-2.jpg",
+        "4" : "dear-3.jpg",
+        "6" : "dear-4.jpg",
+        "8" : "dear-5.jpg",
+        "10" : "dear-6.jpg",
+        "12" : "dear-7.jpg",
+        "14" : "dear-8.jpg",
+        "16" : "dear-9.jpg",
+        "18" : "dear-10.jpg",
+        "20" : "dear-11.jpg", 
+        "22" : "dear-12.jpg",
+    }
+
+    const url = "../test_assets/background_images/Lakeside/"
+
+    // document.body.style.backgroundImage = `url(${url + imageList[0]})`
+
+    let lastBracket = null
+
+    const getBracket = () => {
+        const bracketKeys = Object.keys(imageList).map(Number).sort((a,b) => a-b)
+        const hourOfDay = new Date().getHours()
+
+        if (bracketKeys.includes(hourOfDay)) return hourOfDay
+
+        // Find the largest key smaller than hourOfDay
+        for (let i = 0; i < bracketKeys.length; i++) {
+            if (bracketKeys[i] > hourOfDay) {
+                return bracketKeys[i - 1] !== undefined ? bracketKeys[i - 1] : bracketKeys[0]
+            }
+        }
+
+        // If hourOfDay is larger than all keys, pick the last key
+        return bracketKeys[bracketKeys.length - 1]
+    }
+
+    const update = () => {
+        const currentBracket = getBracket()
+        
+        if (currentBracket === lastBracket) return
+        document.body.style.backgroundImage = `url(${url + imageList[currentBracket]})`
+        lastBracket = currentBracket 
+    }
+
+    return {update}
+}
+
+const BG = setBackground()
+BG.update()
 
 // ********************************Clock********************************
 const rendertime = () => {
@@ -10,6 +64,7 @@ const rendertime = () => {
         minute: '2-digit',
         })
 
+        updateBG()
     }
 
     updateTime()
